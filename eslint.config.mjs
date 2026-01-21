36d5ea49bb30cd99fda1d4ebe633725343aa6499
AnatolyPautovDev <pautov.tolick@yandex.by>
2026-01-21 17:44:55 +0300

    init: husky and lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
+import tanstackQuery from "@tanstack/eslint-plugin-query";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...tanstackQuery.configs["flat/recommended"],
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
