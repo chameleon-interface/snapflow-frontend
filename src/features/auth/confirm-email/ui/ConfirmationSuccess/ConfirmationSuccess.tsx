6e1c4e2020e0cf8bc96c62857372ff6ea3f4af11
AnatolyPautovDev <pautov.tolick@yandex.by>
2026-02-08 13:01:03 +0300

    feat: add translations to ConfirmationSuccess

--- src/features/auth/confirm-email/ui/ConfirmationSuccess/ConfirmationSuccess.tsx
+++ src/features/auth/confirm-email/ui/ConfirmationSuccess/ConfirmationSuccess.tsx
@@ -1,21 +1,24 @@
 import { Button, Typography } from 'snapflow-ui-kit';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useTranslations } from 'next-intl';
 import s from './ConfirmationSuccess.module.css';
 
 export const ConfirmationSuccess = () => {
+  const t = useTranslations();
+
   return (
     <article className={s.wrapper}>
       <div className={s.textWrapper}>
         <Typography variant={'h1'} as={'h1'} className={s.title}>
-          Congratulations!
+          {t('ConfirmationSuccess.title')}
         </Typography>
         <Typography variant={'text-16'}>
-          Your email has been confirmed
+          {t('ConfirmationSuccess.description')}
         </Typography>
       </div>
       <Button as={Link} href={'/sign-in'} className={s.link}>
-        Sign in
+        {t('Auth.logIn')}
       </Button>
       <Image
         src={'/images/bro.svg'}
