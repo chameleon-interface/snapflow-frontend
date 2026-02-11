5a7ab9bf3b4b17bb8cfb21557332002f49da8719
AnatolyPautovDev <pautov.tolick@yandex.by>
2026-02-11 19:10:28 +0300

    feat: add emailNotRegistered server error handling

--- src/features/auth/confirm-email/model/serverErrorMap.ts
+++ src/features/auth/confirm-email/model/serverErrorMap.ts
@@ -2,4 +2,6 @@ export const serverErrorMap: Record<string, string> = {
   'User with this email already confirmed':
     'ServerErrors.emailAlreadyConfirmed',
   'User with this email not found': 'ServerErrors.emailNotFound',
+  'It is impossible to send the code — the specified email is not registered':
+    'ServerErrors.emailNotRegistered',
 };
