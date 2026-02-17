e5fd5cfc287c4e6d227870813470294fb96259a2
Dmitry <kokovin.02@inbox.ru>
2026-02-17 20:51:57 +0300

    Added a modal window when trying to log out

--- src/features/auth/logout/ui/LogoutButton.tsx
+++ src/features/auth/logout/ui/LogoutButton.tsx
@@ -1,24 +1,59 @@
 ﻿'use client';
 
+import { useState } from 'react';
 import { Button } from 'snapflow-ui-kit';
+import { Modal } from 'snapflow-ui-kit/client';
 import { LogOutIcon } from 'snapflow-ui-kit/icons';
 import { useTranslations } from 'next-intl';
-import { useLogoutMutation } from '@/features/auth/logout/api/useLogoutMutation';
 import s from './LogoutButton.module.css';
 
+import { useLogoutMutation } from '@/features/auth/logout/api/useLogoutMutation';
+import { useMe } from '@/entities/user';
+
 export const LogoutButton = () => {
-  const t = useTranslations('Auth');
+  const t = useTranslations();
+  const { data: user } = useMe();
   const logoutMutation = useLogoutMutation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+
+  const handleConfirm = () => {
+    logoutMutation.mutate();
+    setIsOpen(false);
+  };
 
   return (
-    <Button
-      className={s.button}
-      icon={<LogOutIcon />}
-      variant="text"
-      onClick={() => logoutMutation.mutate()}
-      disabled={logoutMutation.isPending}
-    >
-      {t('logOut')}
-    </Button>
+    <>
+      <Button
+        icon={<LogOutIcon />}
+        variant="text"
+        onClick={handleOpen}
+        disabled={logoutMutation.isPending}
+        className={s.button}
+      >
+        {t('Auth.logOut')}
+      </Button>
+
+      <Modal
+        open={isOpen}
+        onClose={handleClose}
+        title={t('Modals.LogoutConfirm.title')}
+      >
+        <p>
+          {t('Modals.LogoutConfirm.message', { email: user?.email as string })}
+        </p>
+
+        <div className={s.modalButtons}>
+          <Button variant="text" onClick={handleClose}>
+            {t('Modals.LogoutConfirm.no')}
+          </Button>
+          <Button onClick={handleConfirm} disabled={logoutMutation.isPending}>
+            {t('Modals.LogoutConfirm.yes')}
+          </Button>
+        </div>
+      </Modal>
+    </>
   );
 };
